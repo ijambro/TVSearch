1553f1910a8fb54647815f06e984e6cad7f5ba95
palmerja@us.ibm.com <palmerja@us.ibm.com@192.168.0.3>
2019-05-29 23:13:26 -0400

    Create TivoShowLoader, using OAuth

---
+++ src/main/java/com/tivo/demo/tvshow/loaders/TivoShowLoader.java
@@ -0,0 +1,88 @@
+package com.tivo.demo.tvshow.loaders;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.lucene.index.IndexWriter;
+import org.json.JSONArray;
+
+import com.tivo.demo.tvshow.loaders.oauth.OAuthSignatureGenerator;
+
+public class TivoShowLoader extends AbstractShowLoader {
+
+	private final String TIVO_DATA_SERVICE_ID_USEAST = "4068858495";
+	private final String TIVO_DATA_SERVICE_CHANNELS_SCREEN = "https://developers.tivo.com/api/v1/resolve/3/data_service_channels/screen?id=";
+	
+	public TivoShowLoader() throws IOException {
+		
+	}
+
+	
+	@Override
+	public JSONArray getShowListing() throws IOException {
+		JSONArray shows = null;
+		
+		String url = TIVO_DATA_SERVICE_CHANNELS_SCREEN + TIVO_DATA_SERVICE_ID_USEAST;
+		System.out.println("Querying Tivo show listing URL: " + url);
+        System.out.println();
+
+        OAuthSignatureGenerator oauth = new OAuthSignatureGenerator();
+        String consumerKey = "5408e9916958a999a2ab762bde87f0b4f4c5ba4b116bc5e5e08f4cf2100c0c90";
+        String consumerSecret = "f0dd1f7b19ba18d3e333a3b011926b83214e7154579f9eaad434994c4e8d3c88";
+        
+        String oauthHeader = null;
+        try {
+        	String signature = oauth.generateSignature("GET", url, consumerKey, consumerSecret);
+        	oauthHeader = oauth.buildAuthorizationHeader(consumerKey, signature);
+        	
+        	System.out.println("OAuth header: " + oauthHeader);
+            System.out.println();
+        } catch (UnsupportedEncodingException | NoSuchAlgorithmException | InvalidKeyException e) {
+        	System.err.println("Exception caught generating OAuth signature: " + e);
+        	e.printStackTrace();
+        	throw new IOException(e);
+        }
+        
+        URL u = new URL(url);
+        HttpURLConnection conn = (HttpURLConnection) u.openConnection();
+        
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("content-type",	"application/json");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setRequestProperty("Authorization", oauthHeader);
+        
+        conn.connect();
+        
+        System.out.println("Response code: " + conn.getResponseCode());
+        System.out.println("Response message: " + conn.getResponseMessage());
+        
+        String jsonResponse = null;
+        
+        if (conn.getResponseCode() == 200) {
+        	jsonResponse = extractResponse(conn);
+        	
+            System.out.println("Response content: " + jsonResponse);
+            
+            shows = new JSONArray(jsonResponse);
+            
+            System.out.println("JSON Array length: " + shows.length());
+        }
+        else {
+            System.out.println("Request failed with response code: " + conn.getResponseCode());
+        }
+        
+		return shows;
+	}
+
+	
+	@Override
+	public void loadShowsIntoIndex(JSONArray allShowsJson, IndexWriter indexWriter) throws IOException {
+		
+
+	}
+
+}
